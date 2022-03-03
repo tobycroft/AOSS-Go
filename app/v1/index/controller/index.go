220faf0b5363c85d5c63a7ec12e887dbe7492320
Tuuz <silverhawk@foxmail.com>
2022-03-03 14:30:35 +0800

    TuuzMac

--- app/v1/index/controller/index.go
+++ app/v1/index/controller/index.go
@@ -1,10 +1,8 @@
 package controller
 
 import (
-	"fmt"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
 	"github.com/gin-gonic/gin"
-	"github.com/tobycroft/gorose-pro"
 	"main.go/app/v1/index/model/AttachmentModel"
 	"main.go/app/v1/index/model/ProjectModel"
 	"main.go/tuuz/Input"
@@ -15,8 +13,7 @@ import (
 
 func IndexController(route *gin.RouterGroup) {
 	route.Any("", index)
-	route.Any("login", loginss)
-	route.Any("upload", upload)
+	route.Any("up", up)
 	route.Any("register")
 }
 
@@ -24,31 +21,12 @@ func index(c *gin.Context) {
 	c.String(0, "index")
 }
 
-func loginss(c *gin.Context) {
-	password := c.Query("password")
-	username := c.Query("username")
-	json := map[string]string{}
-	json["username"] = username
-	json["password"] = password
-	gorose.Open()
-	c.JSON(0, json)
+func up(c *gin.Context) {
+	upload_file(c, false)
 }
 
-func up(c *gin.Context) {
-	client, err := oss.New("Endpoint", "AccessKeyId", "AccessKeySecret")
-	if err != nil {
-		// HandleError(err)
-	}
-
-	bucket, err := client.Bucket("my-bucket")
-	if err != nil {
-		// HandleError(err)
-	}
-
-	err = bucket.PutObjectFromFile("my-object", "LocalFile")
-	if err != nil {
-		// HandleError(err)
-	}
+func up_full(c *gin.Context) {
+	upload_file(c, true)
 }
 
 func upload_file(c *gin.Context, is_full bool) {
